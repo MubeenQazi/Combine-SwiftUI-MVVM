6aba8ca67ff1552e6adb1187373186f1204451a2
Mubeen Raza Qazi <>
2020-01-14 17:25:00 +0500

    Card View Added

--- DynamicList-SwiftUI-Combine/Views/SearchUserView.swift
+++ DynamicList-SwiftUI-Combine/Views/SearchUserView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct SearchUserView: View {
     @ObservedObject var viewModel = SearchUserViewModel()
-    
+    @State private var showingSheet = false
     var body: some View {
         VStack {
             SearchUserBar(text: $viewModel.name) {
@@ -20,7 +20,57 @@ struct SearchUserView: View {
             List(viewModel.users) { user in
                 SearchUserRow(viewModel: self.viewModel, user: user)
                     .onAppear { self.viewModel.fetchImage(for: user) }
-            }.listRowBackground(Color.white)
+                    .gesture(TapGesture()
+                    .onEnded({ _ in
+                        self.showingSheet.toggle()
+                        
+                    })
+                ).sheet(isPresented: self.$showingSheet) {
+                    CardView(viewModel: self.viewModel, user: user)
+                }
+                
+                }.listRowBackground(Color.white)
+                
         }
+    }
+}
+
+struct CardView: View {
+    @ObservedObject var viewModel: SearchUserViewModel
+    @State var user: User
+    @State private var half = false
+    @State private var dim = false
+    var body: some View {
+        VStack {
+            viewModel.userImages[user].map { image in
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.purple, lineWidth: 2))
+                    .scaleEffect(half ? 0.5 : 1.0)
+                    .opacity(dim ? 0.2 : 1.0)
+                    .animation(.easeInOut(duration: 1.0))
+                    .onTapGesture {
+                        self.dim.toggle()
+                        self.half.toggle()
+                    }
+            }
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(user.login)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                }
+                .layoutPriority(100)
+         
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+        .padding([.top, .horizontal])
     }
 }
